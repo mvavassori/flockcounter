9612ec1ce5b93d10a6f6e7d89d301c6cf7e5ca36
Marco Vavassori <mv@marcovavassori.com>
2024-06-17 14:22:16 +0200

    unneessary file for just 2 small functions

--- utils/misc.go
+++ utils/misc.go
@@ -5,12 +5,45 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"sort"
+	"strconv"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/mileusna/useragent"
 )
+
+func ExtractIDFromURL(r *http.Request) (int, error) {
+	vars := mux.Vars(r)
+	idStr, ok := vars["id"]
+	if !ok {
+		return 0, errors.New("ID not provided in the URL")
+	}
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return 0, errors.New("ID must be a number")
+	}
+
+	if id <= 0 {
+		return 0, errors.New("ID must be greater than zero")
+	}
+
+	return id, nil
+}
+
+func ExtractDomainFromURL(r *http.Request) (string, error) {
+	vars := mux.Vars(r)
+
+	domain, ok := vars["domain"]
+	if !ok {
+		return "", errors.New("domain not provided in the URL")
+	}
+
+	return domain, nil
+}
 
 func GetDeviceType(ua *useragent.UserAgent) string {
 	if ua.Mobile {
