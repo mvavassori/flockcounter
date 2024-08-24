b0f240cb47e22c8bcca863eefbc19854f97b3645
Marco Vavassori <mv@marcovavassori.com>
2024-08-24 23:30:52 +0200

    payment json marshallling

--- handlers/payment_handler.go
+++ handlers/payment_handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,51 +12,46 @@ import (
 	"github.com/stripe/stripe-go/v79/customer"
 )
 
-// The init function is a special function in Go that runs automatically when the package is initialized
 func init() {
 	stripe.Key = "sk_test_51ONYpVEjL7fX4p99WhzOhVfRqbdGmvYlI37v6tkSThMAYJZJ5CVIhZSU6UWzVCH1AyIMk8ocxp1A56fFrNSSjzXn00JAfKJEsm"
 }
 
 func CreateCheckoutSession(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
 		fmt.Println("CreateCheckoutSession")
 
-		// extract data from json body // todo: change it to model?
 		var req struct {
 			Email  string `json:"email"`
 			UserID string `json:"userId"`
 			Plan   string `json:"plan"`
 		}
 
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
+			return
+		}
+
 		if req.Plan == "basic" {
 			req.Plan = "price_1Ppu8VEjL7fX4p99LqYqruOC"
 		}
 
-		// // Define discounts slice
-		// discounts := []*stripe.CheckoutSessionDiscountParams{	// slice of pointers to stripe.CheckoutSessionDiscountParams
-		// 	{
-		// 		PromotionCode: stripe.String("PROMO_CODE"),
-		// 	},
-		// }
-
 		customerParams := &stripe.CustomerParams{
 			Email: stripe.String(req.Email),
 		}
-
 		customerParams.AddMetadata("FinalEmail", req.Email)
+
 		newCustomer, err := customer.New(customerParams)
 		if err != nil {
 			log.Printf("customer.New: %v", err)
+			http.Error(w, "Error creating customer", http.StatusInternalServerError)
+			return
 		}
 
 		params := &stripe.CheckoutSessionParams{
 			Customer: &newCustomer.ID,
 			LineItems: []*stripe.CheckoutSessionLineItemParams{
-				// &stripe.CheckoutSessionLineItemParams
 				{
-					// Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-					Price:    stripe.String(req.Plan), // basic plan test
+					Price:    stripe.String(req.Plan),
 					Quantity: stripe.Int64(1),
 				},
 			},
@@ -63,12 +59,16 @@ func CreateCheckoutSession(db *sql.DB) http.HandlerFunc {
 			SuccessURL:   stripe.String("http://localhost:3000/success"),
 			CancelURL:    stripe.String("http://localhost:3000/canceled"),
 			AutomaticTax: &stripe.CheckoutSessionAutomaticTaxParams{Enabled: stripe.Bool(true)},
+			CustomerUpdate: &stripe.CheckoutSessionCustomerUpdateParams{
+				Address: stripe.String("auto"),
+			},
 		}
 
 		s, err := session.New(params)
-
 		if err != nil {
 			log.Printf("session.New: %v", err)
+			http.Error(w, "Error creating checkout session", http.StatusInternalServerError)
+			return
 		}
 
 		http.Redirect(w, r, s.URL, http.StatusSeeOther)
